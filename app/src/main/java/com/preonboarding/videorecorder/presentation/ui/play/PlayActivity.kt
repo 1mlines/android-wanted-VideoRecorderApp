b423e11219c284c131f4fb412401d9ab2f9c7ffe
leehandsub <gustjq7027@naver.com>
2022-10-20 00:50:17 +0900

    feat - play,record 액티비티 생성

---
+++ app/src/main/java/com/preonboarding/videorecorder/presentation/ui/play/PlayActivity.kt
@@ -0,0 +1,12 @@
+package com.preonboarding.videorecorder.presentation.ui.play
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.preonboarding.videorecorder.R
+
+class PlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+    }
+}
