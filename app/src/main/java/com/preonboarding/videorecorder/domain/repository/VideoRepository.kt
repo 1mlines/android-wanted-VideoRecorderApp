4a59d5ef42f8a6df47e9247713d14e6c96da3e42
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-10-22 17:33:23 +0900

    fix - 레파지토리에서 비디오를 불러올 때 Flow 로 불러오도록 수정

--- app/src/main/java/com/preonboarding/videorecorder/domain/repository/VideoRepository.kt
+++ app/src/main/java/com/preonboarding/videorecorder/domain/repository/VideoRepository.kt
@@ -1,9 +1,10 @@
 package com.preonboarding.videorecorder.domain.repository
 
 import com.preonboarding.videorecorder.domain.model.Video
+import kotlinx.coroutines.flow.Flow
 
 interface VideoRepository {
     suspend fun saveVideo(video: Video)
     suspend fun deleteVideo(video: Video)
-    suspend fun getVideoList(): List<Video>
+    suspend fun getVideoList(): Flow<List<Video>>
 }
