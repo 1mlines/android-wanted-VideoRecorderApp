1c653e10acd92dc48419c8c2feb417dc355ba94d
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2022-10-22 17:58:22 +0900

    refactor - StateFlow 를 통해 비디오 리스트를 받아오도록 수정

--- app/src/main/java/com/preonboarding/videorecorder/presentation/ui/main/MainViewModel.kt
+++ app/src/main/java/com/preonboarding/videorecorder/presentation/ui/main/MainViewModel.kt
@@ -1,51 +1,47 @@
 package com.preonboarding.videorecorder.presentation.ui.main
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.preonboarding.videorecorder.domain.model.Video
 import com.preonboarding.videorecorder.domain.usecase.DeleteVideoUseCase
 import com.preonboarding.videorecorder.domain.usecase.GetVideoListUseCase
-import com.preonboarding.videorecorder.domain.usecase.SaveVideoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val getVideoListUseCase: GetVideoListUseCase,
-    private val saveVideoUseCase: SaveVideoUseCase,
     private val deleteVideoUseCase: DeleteVideoUseCase,
 ) : ViewModel() {
-    private val _videoList = MutableLiveData<List<Video>>().apply {
-        value =
-            listOf(
-                Video(1, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4", "2020-02-22"),
-                Video(2, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4", "2020-02-22"),
-                Video(3, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4", "2020-02-22"),
-                Video(4, "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4", "2020-02-22")
-            )
-    }
-    val videoList: LiveData<List<Video>> get() = _videoList
 
-    fun getVideoList() {
-        viewModelScope.launch {
-            _videoList.postValue(getVideoListUseCase.invoke())
-        }
+    private val _videoList = MutableStateFlow<List<Video>>(emptyList())
+    val videoList: StateFlow<List<Video>> get() = _videoList
+
+    init {
+        getVideoList()
     }
 
-    fun saveVideo(video: Video) {
+    private fun getVideoList() {
         viewModelScope.launch {
-            saveVideoUseCase.invoke(video)
+            getVideoListUseCase.invoke().stateIn(
+                viewModelScope,
+                SharingStarted.Lazily,
+                emptyList()
+            ).collectLatest {
+                _videoList.emit(it)
+            }
         }
     }
 
     fun deleteVideo(video: Video) {
         viewModelScope.launch {
-            Timber.tag("asdf").e(video.id.toString())
-            //deleteVideoUseCase.invoke(video)
+            deleteVideoUseCase.invoke(video)
         }
     }
 }
